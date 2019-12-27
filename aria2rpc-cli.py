ba40f76fb00b542ad5665e5c0c061dfbedad901f
Ken Chou <zhang-j@klab.com>
2019-12-27 11:39:36 +0800

    refactor

--- aria2rpc-cli.py
+++ aria2rpc-cli.py
@@ -10,8 +10,9 @@ from pathlib import Path
 from fnmatch import fnmatch
 from torrent_parser import TorrentFileParser, InvalidTorrentDataException
 
-from aria2rpc import Aria2RpcClient, print_status, get_config, guess_path, \
-    DEFAULT_CONFIG_PATH, DEFAULT_ARIA2_CONFIG, DEFAULT_ARIA2_JSONRPC, DEFAULT_TORRENT_EXCLUDE_LIST_FILE
+from aria2rpc import Aria2RpcClient, \
+    print_response_status, get_config, guess_path, \
+    LOG_LEVELS, DEFAULT_CONFIG_PATH, DEFAULT_ARIA2_CONFIG, DEFAULT_ARIA2_JSONRPC, DEFAULT_TORRENT_EXCLUDE_LIST_FILE
 
 
 PATTERN_SUPPORTED_URI = re.compile('(http(s)?|ftp(s)|sftp)://|magnet:')
@@ -63,34 +64,34 @@ def build_exclude_list(filename):
 
 
 @click.group()
-@click.option('--config-file', default=DEFAULT_ARIA2_CONFIG,
+@click.option('--config-file', type=click.Path(exists=False),
               help=f'config of Aria2 JSON-RPC server. '
-                   'if provide both --conf and --json-rpc/--token, prefers to use --json-rpc/--token',
+                   'if provide both --config-file and --json-rpc/--token, prefers to use --json-rpc/--token',
               show_default=True)
 @click.option('--json-rpc', help='Aria2 JSON-RPC server.')
 @click.option('--token', help='RPC SECRET string.')
-@click.option('--debug', is_flag=True, default=False, help="enable debug mode")
+@click.option('-v', '--verbose', count=True, help='Increase output verbosity.')
 @click.pass_context
-def cli(ctx, config_file, json_rpc, token, debug):
+def cli(ctx, config_file, json_rpc, token, verbose):
     """Aria2 RPC Client"""
-    logging.basicConfig(level=logging.DEBUG if debug else logging.WARNING)
+    logging.basicConfig(level=LOG_LEVELS.get(verbose, logging.INFO))
     # init logger
-    logging.basicConfig(level=logging.WARNING)
     logger = logging.getLogger(__name__)
     click_log.basic_config(logger)
 
     guess_paths = [
-        Path.cwd(),  # current dir ./
-        Path.home() / DEFAULT_CONFIG_PATH,    # ~/.aria2/
+        Path('.'),  # current dir ./
+        Path.home() / DEFAULT_CONFIG_PATH,  # ~/.aria2/
         Path(__file__).resolve().parent / DEFAULT_CONFIG_PATH,  # ${BIN_PATH}/.aria2/
     ]
-    config_file_path = guess_path(config_file or DEFAULT_ARIA2_CONFIG, guess_paths)
-    if config_file_path is None:
-        config = {'json-rpc': DEFAULT_ARIA2_JSONRPC}
-        if config_file:     # warning if provided the config_file option but not found
-            click.secho(f'Warning: config {config_file} not found in paths: {guess_paths}, ', err=True, fg='yellow')
+    if config_file is not None:
+        config_file_path = guess_path(config_file, guess_paths)
+        if config_file_path is None:
+            logger.error(f'--config-file "{config_file}" not found in paths: {[str(p) for p in guess_paths]}')
+            exit(1)
     else:
-        config = get_config(config_file_path)
+        config_file_path = guess_path(DEFAULT_ARIA2_CONFIG, guess_paths)
+    config = get_config(config_file_path) or {'json-rpc': DEFAULT_ARIA2_JSONRPC}
 
     if not json_rpc:
         json_rpc = config.get('json-rpc', DEFAULT_ARIA2_JSONRPC)
@@ -126,7 +127,8 @@ def add(ctx, download_dir, exclude_file, set_pause, torrent_files_or_uris):
     click.echo(f'*        pause: {set_pause}')
     click.echo(f'*        files: {torrent_files_or_uris}')
 
-    exclude_file_path = guess_path(exclude_file, guess_paths) or guess_path(DEFAULT_TORRENT_EXCLUDE_LIST_FILE, guess_paths)
+    exclude_file_path = guess_path(exclude_file, guess_paths) or guess_path(DEFAULT_TORRENT_EXCLUDE_LIST_FILE,
+                                                                            guess_paths)
     # exclude list
     exclude_patterns = build_exclude_list(exclude_file_path)
 
@@ -152,7 +154,7 @@ def add(ctx, download_dir, exclude_file, set_pause, torrent_files_or_uris):
                     selected = torrent_filter_file(torrent['info'], exclude_patterns)
                     if selected:
                         options['select-file'] = ','.join(selected)
-                    f.seek(0)   # rewind the file
+                    f.seek(0)  # rewind the file
                     # aria2.addTorrent([secret, ]torrent[, uris[, options[, position]]])
                     # @see https://aria2.github.io/manual/en/html/aria2c.html#aria2.addTorrent
                     response = aria2.addTorrent(base64.b64encode(f.read()).decode('utf-8'), [], options)
@@ -180,19 +182,25 @@ def list_queue(ctx, show_all, show_active, show_waiting, show_stopped):
     show_active = show_active or not (show_waiting or show_stopped)
     if show_all or show_active:
         response = aria2.tellActive()
-        print_status(response, title='### Active ###')
+        print_response_status(response, title='### Active ###')
     if show_all or show_waiting:
         response = aria2.tellWaiting(0, 50)
-        print_status(response, title='### Waiting ###')
+        print_response_status(response, title='### Waiting ###')
     if show_all or show_stopped:
         response = aria2.tellStopped(0, 50)
-        print_status(response, title='### Completed/Stopped ###')
+        print_response_status(response, title='### Completed/Stopped ###')
 
 
 @cli.command()
+@click.argument('gid', nargs=-1, required=True)
 @click.pass_context
-def show(ctx):
+def info(ctx, gid):
     """TODO: Show detail info of a task"""
+    gid_list = gid
+    aria2 = ctx.obj['aria2']
+    for gid in gid_list:
+        response = aria2.tellStatus(gid)
+        click.echo(response)
 
 
 @cli.command()
--- aria2rpc/__init__.py
+++ aria2rpc/__init__.py
@@ -1,14 +1,21 @@
 import json
+import logging
 import requests
 
 from baseconv import base36
 from pathlib import Path
+from time import sleep
 
 
 DEFAULT_CONFIG_PATH = '.aria2'
 DEFAULT_ARIA2_CONFIG = 'aria2rpc.json'
 DEFAULT_TORRENT_EXCLUDE_LIST_FILE = 'torrent_clean.lst'
 DEFAULT_ARIA2_JSONRPC = 'http://localhost:6800/jsonrpc'
+LOG_LEVELS = {
+    0: logging.WARNING,
+    1: logging.INFO,
+    2: logging.DEBUG,
+}
 
 
 def uniq_list_keep_order(seq):
@@ -22,13 +29,14 @@ def guess_path(test_file, guess_paths=None):
     """test the file exists in one of guess paths"""
     if test_file is None:
         return
+    test_file = Path(test_file).expanduser()
     if guess_paths is None:
         guess_paths = [
             Path.cwd(),  # current dir
             Path.home(),  # home dir
             Path(__file__).parent.parent,  # script dir
         ]
-    for p in uniq_list_keep_order(guess_paths):
+    for p in uniq_list_keep_order([Path(p).resolve() for p in guess_paths]):
         file_path = p / test_file
         if file_path.is_file():
             return file_path
@@ -36,9 +44,27 @@ def guess_path(test_file, guess_paths=None):
 
 
 def get_config(filename):
-    with open(filename) as f:
-        config = json.load(f)
-        return config
+    if filename is None:
+        return None
+    try:
+        with open(filename) as f:
+            config = json.load(f)
+            return config
+    except IOError as e:
+        logging.WARNING(e)
+        return None
+
+
+def try_call(func, *args, **kwargs):
+    _MAX_DELAY_TIME = 5
+    _sleep_time = 0
+    _retry = True
+    while _retry:
+        _res = func(*args, **kwargs)
+        _retry = _res.error
+        if _retry:
+            _sleep_time += 1 if _sleep_time <= _MAX_DELAY_TIME else 0
+            sleep(_sleep_time)
 
 
 class Aria2RpcClient:
@@ -93,6 +119,36 @@ class Aria2RpcResponse:
         return json.dumps(self.response)
 
 
+class Aria2QueueManager:
+    """Queue Manager"""
+    def __init__(self, aria2rpc):
+        self.queue = []
+        self.statistics = {}
+        self.aria2rpc = aria2rpc
+
+    def update(self, task_list):
+        self.queue = []
+        aria2 = self.aria2rpc
+        for task in task_list:
+            gid = task['gid']
+            completed_length = int(task['completedLength'])
+            download_speed = task['downloadSpeed']
+            total_length = int(task['totalLength'])
+            s = self.statistics.setdefault(gid, {'completed-length': 0})
+            prev_length = s.get('completed-length', 0)
+            if completed_length - prev_length == 0:
+                # TODO: compute avg download speed
+                try_call(aria2.pause, gid)
+                try_call(aria2.unpause, gid)
+                self.queue.append(gid)
+            s['completed-length'] = completed_length
+        position = 1000
+        for gid in self.queue:
+            position += 1
+            r = aria2.changePosition(gid, position, 'POS_SET')
+            logging.info('%s(%s, %s, %s): %s', 'changePosition', gid, position, 'POS_SET', r)
+
+
 def get_task_name(task):
     if 'bittorrent' in task:
         return task['bittorrent']['info']['name']
@@ -102,26 +158,34 @@ def get_task_name(task):
         return None
 
 
-def print_status(response, title=None):
+def print_task_status_header():
+    print('{:16}\t{}\t{:12}\t{:12}\t{:9}(%)\t{}'.format('GID', 'Completed', 'Total', 'Progress', 'Speed', 'Task_Name'))
+
+
+def print_task_status(task):
+    gid = task['gid']
+    task_name = get_task_name(task)
+    completed_length = int(task['completedLength'])
+    download_speed = task['downloadSpeed']
+    total_length = int(task['totalLength'])
+    print('{}\t{:12}\t{:12}\t{:9.2f}%\t{:9d}\t{}'.format(
+        gid,
+        completed_length,
+        total_length,
+        completed_length / total_length * 100 if total_length else 0,
+        int(download_speed),
+        task_name,
+    ))
+
+
+def print_response_status(response, title=None, callback=None):
     if response.error:
         print(response.error)
         return
     if title:
         print(title)
-    # print(response)
-    print('{:16}\t{}\t{:12}\t{:12}\t{:9}(%)\t{}'.format('GID', 'Completed', 'Total', 'Progress', 'Speed', 'Task_Name'))
+    print_task_status_header()
     for task in response.result:
-        # print('>>> task', json.dumps(task))
-        gid = task['gid']
-        task_name = get_task_name(task)
-        completed_length = int(task['completedLength'])
-        download_speed = task['downloadSpeed']
-        total_length = int(task['totalLength'])
-        print('{}\t{:12}\t{:12}\t{:9.2f}%\t{:9d}\t{}'.format(
-            gid,
-            completed_length,
-            total_length,
-            completed_length / total_length * 100 if total_length else 0,
-            int(download_speed),
-            task_name,
-        ))
+        print_task_status(task)
+        if callback is not None:
+            callback()
