13b69ccdae785e4338a701b06883492be0a5b8c9
Ken Chou <zhang-j@klab.com>
2020-11-22 22:40:49 +0800

    use aria2p as RPC client library

--- aria2-oversee.py
+++ aria2-oversee.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import aria2p
 import click
 import click_log
 import logging
@@ -7,20 +8,22 @@ import logging
 from pathlib import Path
 from time import sleep
 
-from aria2rpc import Aria2RpcClient, Aria2QueueManager, \
-    print_response_status, get_config, guess_path, \
-    LOG_LEVELS, DEFAULT_CONFIG_PATH, DEFAULT_ARIA2_CONFIG, DEFAULT_ARIA2_JSONRPC
+from aria2rpc import Aria2QueueManager, \
+    get_config, guess_path, \
+    LOG_LEVELS, DEFAULT_CONFIG_PATH, DEFAULT_ARIA2_CONFIG, DEFAULT_ARIA2_HOST, DEFAULT_ARIA2_PORT
 
 
 @click.command()
 @click.option('--config-file', default=DEFAULT_ARIA2_CONFIG, type=click.Path(),
-              help=f'config of Aria2 JSON-RPC server. '
+              help='config of Aria2 JSON-RPC server. '
                    'if provide both --config-file and --json-rpc/--token, prefers to use --json-rpc/--token',
               show_default=True)
-@click.option('--json-rpc', help='Aria2 JSON-RPC server. default: {}'.format(DEFAULT_ARIA2_JSONRPC))
+@click.option('--host', help='Aria2 JSON-RPC server host. default: {}'.format(DEFAULT_ARIA2_HOST))
+@click.option('--port', help='Aria2 JSON-RPC server port. default: {}'.format(DEFAULT_ARIA2_PORT))
 @click.option('--token', help='RPC SECRET string')
+@click.option('-t', '--interval', default=300, help='Check interval')
 @click.option('-v', '--verbose', count=True, help='Increase output verbosity.')
-def run(config_file, json_rpc, token, verbose):
+def run(config_file, host, port, token, interval, verbose):
     logging.basicConfig(level=LOG_LEVELS.get(verbose, logging.INFO))
     logger = logging.getLogger(__name__)
     click_log.basic_config(logger)
@@ -32,28 +35,19 @@ def run(config_file, json_rpc, token, verbose):
     config_file_path = guess_path(config_file, guess_paths) or guess_path(DEFAULT_ARIA2_CONFIG, guess_paths)
     config = get_config(config_file_path)
 
-    if not json_rpc:
-        json_rpc = config.get('json-rpc', DEFAULT_ARIA2_JSONRPC)
-    if not token:
-        token = config.get('token')
-
-    aria2 = Aria2RpcClient(url=json_rpc, token=token)
+    aria2 = aria2p.API(
+        aria2p.Client(
+            host=host or config.get('host', DEFAULT_ARIA2_HOST),
+            port=port or config.get('port', DEFAULT_ARIA2_PORT),
+            secret=token or config.get('token')
+        )
+    )
     aria2_queue_manager = Aria2QueueManager(aria2)
 
     while True:
-        # has waiting tasks?
-        waiting = aria2.tellWaiting(0, 1)
-        if True or len(waiting.result):
-            response = aria2.tellActive()
-            print_response_status(response)
-            if not response.error:
-                aria2_queue_manager.update(response.result)
-            waiting = aria2.tellWaiting(0, 20)
-            print_response_status(waiting, title='### Waiting ###')
-        else:
-            logger.info('No waiting tasks in queue.')
-        logger.info('sleep 300s.')
-        sleep(300)
+        aria2_queue_manager.run()
+        logger.info(f'sleep {interval}s.')
+        sleep(interval)
 
 
 if __name__ == "__main__":
