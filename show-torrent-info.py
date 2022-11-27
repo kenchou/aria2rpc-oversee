23ca98ae6b7a60fdeba574530e1069e5c25024d6
Ken Chou <zhang-j@klab.com>
2022-11-27 16:47:32 +0800

    add --dry-run, format and refactor.

--- show-torrent-info.py
+++ show-torrent-info.py
@@ -3,35 +3,37 @@
 import click
 import json
 import math
-import os.path
 import torrent_parser as tp
 
+from pathlib import Path
 
-def sizeof_fmt(num, suffix='B'):
+
+units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+
+
+def sizeof_fmt(num):
     magnitude = int(math.floor(math.log(num, 1024)))
     val = num / math.pow(1024, magnitude)
-    if magnitude > 7:
-        return '{:.1f}{}{}'.format(val, 'Yi', suffix)
-    return '{:3.1f}{}{}'.format(val, ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi'][magnitude], suffix)
+    unit = units[min(magnitude, len(units) - 1)]
+    return f"{val:4.1f}{unit:<3}"
 
 
 @click.command()
-@click.option('-l', '--file-list', is_flag=True, help='File list.')
-@click.argument('url-or-torrent-path', nargs=-1, required=True)
+@click.option("-l", "-ls", "--file-list", is_flag=True, help="File list.")
+@click.argument("url-or-torrent-path", nargs=-1, required=True)
 def main(file_list, url_or_torrent_path):
     for uri in url_or_torrent_path:
         torrent = tp.parse_torrent_file(uri)
         if file_list:
-            if 'files' in torrent['info']:
-                for file_info in torrent['info']['files']:
-                    include_path = len(file_info['path']) > 1
-                    file_path = os.path.join(*file_info['path'])
+            if "files" in torrent["info"]:
+                for file_info in torrent["info"]["files"]:
+                    file_path = Path(*file_info["path"])
                     file_length = file_info["length"]
-                    click.echo(f'{os.path.basename(uri)}\t{sizeof_fmt(file_length)}\t{file_path}')
+                    click.echo(f"{Path(uri).name}\t{sizeof_fmt(file_length):>8}\t{file_path}")
             pass
         else:
             print(json.dumps(torrent, sort_keys=True, indent=4))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
