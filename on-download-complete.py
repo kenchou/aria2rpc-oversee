7b5d91469a338e8ba71f117e7b5cde913774cbbc
Ken Chou <zhang-j@klab.com>
2025-05-14 17:08:53 +0800

    sync or move

--- on-download-complete.py
+++ on-download-complete.py
@@ -8,11 +8,16 @@ import subprocess
 
 from pathlib import Path
 
-from aria2rpc import load_aria2_config, \
-    LOG_LEVELS, DEFAULT_ARIA2_CONFIG, DEFAULT_ARIA2_HOST, DEFAULT_ARIA2_PORT
+from aria2rpc import (
+    load_aria2_config,
+    LOG_LEVELS,
+    DEFAULT_ARIA2_CONFIG,
+    DEFAULT_ARIA2_HOST,
+    DEFAULT_ARIA2_PORT,
+)
 
 
-LOG_FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s'
+LOG_FORMAT = "%(asctime)-15s [%(levelname)s] %(message)s"
 logger = logging.getLogger()
 
 
@@ -28,12 +33,14 @@ def print_task_info(task):
     print(f"+ {task.name}")
     print(f"  - {task.error_code=}, {task.error_message}")
     print(f"  - {task.dir}")
-    print(f"  - {task.gid:<17} "
-          f"{task.status:<9} "
-          f"{task.progress_string():>8} "
-          f"{task.download_speed_string():>12} "
-          f"{task.upload_speed_string():>12} "
-          f"{task.eta_string():>8}")
+    print(
+        f"  - {task.gid:<17} "
+        f"{task.status:<9} "
+        f"{task.progress_string():>8} "
+        f"{task.download_speed_string():>12} "
+        f"{task.upload_speed_string():>12} "
+        f"{task.eta_string():>8}"
+    )
 
 
 def on_download_complete(api, gid):
@@ -46,10 +53,12 @@ def on_download_complete(api, gid):
         task.purge()
         return
     # move files from tmp dir to another
-    if '.tmp' == task.dir.name:
+    if ".tmp" == task.dir.name:
         subprocess.call(["chmod", "-R", "g+w", Path(task.dir)])
         destination = Path(task.dir.parent)
-        logger.info(f'Complete {task.gid}: move "{task.name}" from {task.dir} to {destination}')
+        logger.info(
+            f'Complete {task.gid}: move "{task.name}" from {task.dir} to {destination}'
+        )
         if task.move_files(destination):
             control_file = task.control_file_path
             if control_file.exists():
@@ -58,21 +67,43 @@ def on_download_complete(api, gid):
             # task.purge()
 
 
+def move_or_merge(task: aria2p.downloads.Download, destination: Path):
+    all_success = True
+    if destination.exists():  # 目标已存在，使用 rsync
+        for path in task.root_files_paths:
+            exit_code = subprocess.call(["rsync", "-a", path, destination])
+            if exit_code != 0:
+                all_success = False
+        if all_success:
+            for path in task.root_files_paths:
+                path.unlink(missing_ok=True)
+    else:
+        return task.move_files(destination)
+
+
 @click.command()
-@click.argument('gid')
-@click.argument('file-count')
-@click.argument('destination')
-@click.option('--config-file', default=DEFAULT_ARIA2_CONFIG, type=click.Path(),
-              help='config of Aria2 JSON-RPC server. '
-                   'if provide both --config-file and --json-rpc/--token, prefers to use --json-rpc/--token',
-              show_default=True)
-@click.option('--host', help='Aria2 JSON-RPC server host. default: {}'.format(DEFAULT_ARIA2_HOST))
-@click.option('--port', help='Aria2 JSON-RPC server port. default: {}'.format(DEFAULT_ARIA2_PORT))
-@click.option('--token', help='RPC SECRET string')
+@click.argument("gid")
+@click.argument("file-count")
+@click.argument("destination")
+@click.option(
+    "--config-file",
+    default=DEFAULT_ARIA2_CONFIG,
+    type=click.Path(),
+    help="config of Aria2 JSON-RPC server. "
+    "if provide both --config-file and --json-rpc/--token, prefers to use --json-rpc/--token",
+    show_default=True,
+)
+@click.option(
+    "--host", help="Aria2 JSON-RPC server host. default: {}".format(DEFAULT_ARIA2_HOST)
+)
+@click.option(
+    "--port", help="Aria2 JSON-RPC server port. default: {}".format(DEFAULT_ARIA2_PORT)
+)
+@click.option("--token", help="RPC SECRET string")
 def cli(gid, file_count, destination, config_file, host, port, token):
     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
-    fh = logging.FileHandler('/tmp/aria2-event.log')
+    fh = logging.FileHandler("/tmp/aria2-event.log")
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(logging.Formatter(LOG_FORMAT))
     logger.addHandler(fh)
@@ -85,14 +116,14 @@ def cli(gid, file_count, destination, config_file, host, port, token):
     on_download_complete(
         aria2p.API(
             aria2p.Client(
-                host=host or config.get('host'),
-                port=port or config.get('port'),
-                secret=token or config.get('token')
+                host=host or config.get("host"),
+                port=port or config.get("port"),
+                secret=token or config.get("token"),
             )
         ),
-        gid
+        gid,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
